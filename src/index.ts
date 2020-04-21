fa90a533f81de39b2fb1a3e9f9386f1790770511
Alex Karavil <46430499+Karavil@users.noreply.github.com>
2020-04-20 20:23:12 -0400

    Completed register and login functionality for database

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,9 @@
 import { PrismaClient } from "@prisma/client";
 import * as bodyParser from "body-parser";
 import express from "express";
+import bcrypt from "bcryptjs";
+
+import { User } from "@prisma/client";
 
 const prisma = new PrismaClient();
 const app = express();
@@ -8,26 +11,50 @@ const app = express();
 app.use(bodyParser.json());
 
 app.get("/users", async (req, res) => {
-   const users = await prisma.user.findMany();
+   const users: User[] = await prisma.user.findMany();
    res.status(200).json(users);
 });
 
+/**
+ * Route for logins. The user password input will be hashed then compared to the
+ * hashed password on the database.
+ */
 app.post("/auth/login", async (req, res) => {
-   const result = await prisma.user.create({
-      data: {
-         ...req.body,
+   // Find the user by username in database, return
+   const user: User | null = await prisma.user.findOne({
+      where: {
+         username: req.body.username,
       },
    });
-   res.json(result);
+   // Compare the hashes of the passwords if the user is found
+   if (
+      user !== null &&
+      bcrypt.compareSync(req.body.password, user.password) === true
+   ) {
+      res.status(200).json({ message: "Authenticated. Logging you in..." });
+   } else {
+      res.status(401).json({ message: "Incorrect password" });
+   }
 });
 
+// The amount of hashing rounds done on the password, default is 2^12
+const saltRounds = process.env.BCRYPT_SALT_ROUNDS || 12;
+/**
+ * This route is used when a user is registering (new user on the website)
+ * Requires a username and password on the request body
+ */
 app.post("/auth/register", async (req, res) => {
-   const result = await prisma.user.create({
+   // Has the user's preferred password
+   const passwordHash = bcrypt.hashSync(req.body.password, saltRounds);
+   // Push the password (that's hashed) onto the database for future use
+   const newUser = await prisma.user.create({
       data: {
-         ...req.body,
+         username: req.body.username,
+         password: passwordHash,
       },
    });
-   res.json(result);
+   // Send the hashed password back (just for testing, don't do in real projects)
+   res.status(201).json(newUser);
 });
 
 app.listen(5000, () =>
